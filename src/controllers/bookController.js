f503bbaba9f811d0ff78c7aeee7158b6592559f4
Hayat Singh <jeet53316@gmail.com>
2023-06-16 17:11:30 +0530

    feat: Book controller added

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,8 +1,74 @@
+import Books from '../models/book_model';
+import Users from '../models/user_model';
+import { isValid } from '../validations/validations';
+
 export const createBook = async (req, res) => {
+  const data = req.body;
+  const userId = req.userId;
 
-    const data = req.body;
-    const userId = req.userId;
+  try {
+    if (!isValid(userId)) {
+      return res.status(400).send({ status: false, msg: 'userID is required' });
+    }
+    if (!Object.keys(data).length === 0) {
+      return res.status(400).send({ status: false, msg: 'userId is required' });
+    }
 
-    
+    if (!isValid(data.title)) {
+      return res.status(400).send({ status: false, msg: 'title is required' });
+    }
+    if (!isValid(data.excerpt)) {
+      return res
+        .status(400)
+        .send({ status: false, msg: 'excerpt is required' });
+    }
+    if (!isValid(data.ISBN)) {
+      return res.status(400).send({ status: false, msg: 'title is required' });
+    }
+    if (!isValid(data.category)) {
+      return res
+        .status(400)
+        .send({ status: false, msg: 'category is required' });
+    }
+    if (!isValid(data.subcategory)) {
+      return res
+        .status(400)
+        .send({ status: false, msg: 'subcategory is required' });
+    }
 
-}
+    const duplicate = await Books.findOne({ title: data.title });
+    if (duplicate) {
+      return res
+        .status(400)
+        .send({ status: false, msg: 'title already exists' });
+    }
+
+    const duplicateISBN = await Books.findOne({ ISBN: data.ISBN });
+    if (duplicateISBN) {
+      return res
+        .status(400)
+        .send({ status: false, msg: 'ISBN already exists' });
+    }
+
+    const isAuthorPresent = await Users.findOne(userId);
+    if (!isAuthorPresent) {
+      return res
+        .status(400)
+        .send({ status: false, msg: 'Author id not present' });
+    }
+
+    let createBooks = await Books.create(data);
+    if (Object.keys(createBooks).length == 0) {
+      return res
+        .status(400)
+        .send({ status: false, msg: 'please provide some data' });
+    } else {
+      res.status(200).send({ status: true, msg: createBooks });
+    }
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
+
+
+export default  createBook ;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,11 @@
 import express from 'express';
 import { createUser, userLogin } from '../controllers/userController.js';
-import auth from '../auth/auth.js';
+import { createBook } from '../controllers/bookController.js';
 
 const router = express.Router();
 
-router.get('/profile', (req, res) => {
-  res.send('Test');
-});
-
 router.post('/register', createUser);
 router.post('/loginUser', userLogin);
-
-router.post('/userData',auth, );
+router.post('/createBooks', createBook);
 
 export default router;
